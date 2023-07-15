9887b08c97461170f8eff86f7fae8efaec3a6667
JDunc <jjsduncan@gmail.com>
2023-07-15 17:50:43 -0400

    added update routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,7 +37,11 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: { id: req.params.id },
+})
+ .then(data =>  res.status(200).json(data))
+
 });
 
 router.delete('/:id', (req, res) => {
